5b8c4b938315ef45cf343c2703cef2f638705122
rasmus <rasmus.ohrstig@gmail.com>
2019-01-19 17:52:21 +0100

    Adding spotless, applying default klint rules

--- app/src/test/java/com/ohrstigbratt/wordmeanings/ExampleUnitTest.kt
+++ app/src/test/java/com/ohrstigbratt/wordmeanings/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.ohrstigbratt.wordmeanings
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
